9a0e7997022ce335fa140e31286219b4d66d8807
Rabee Aljaramani <rabeejermani@gmail.com>
2022-04-27 12:52:43 +0400

    start

---
+++ src/card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Card() {
+  return (
+    <div className="card-container">
+      <div className="items-container">
+        <div className="card-item">
+          <div className="icon">ICON</div>
+          <div className="text">corporate website </div>
+        </div>
+        <div className="card-item">
+          <div className="icon">ICON</div>
+          <div className="text">corporate Instagram </div>
+        </div>
+        <div className="card-item">
+          <div className="icon">ICON</div>
+          <div className="text">corporate Linkedin</div>
+        </div>
+      </div>
+    </div>
+  );
+}
