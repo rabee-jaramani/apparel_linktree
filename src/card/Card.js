aa6a2122be626fb4ac9e1a332d5982de23fa08a4
Rabee Aljaramani <rabeejermani@gmail.com>
2023-01-10 11:30:23 +0400

    Update Card.js

--- src/card/Card.js
+++ src/card/Card.js
@@ -6,10 +6,6 @@ import careers from '../images/careers.png';
 import app_store from '../images/app_store.png';
 import google_play from '../images/google_play.png';
 import tree from '../images/tree.svg';
-import f5 from '../images/f5.png';
-import yoga from '../images/yoga.png';
-import zumba from '../images/zumba.svg';
-// import ca_logo from '../images/ca_logo.png';
 import ag_logo from '../images/ag_logo.png';
 export default function Card() {
   return (
@@ -114,18 +110,7 @@ export default function Card() {
             <div className="text">Meet our founder Mrs. Sima Ved</div>
           </div>
         </a>
-        <a
-          target="_blank"
-          href="https://www.thenationalnews.com/lifestyle/fashion/2022/08/13/the-16-year-old-behind-f5-the-sustainable-athleisure-brand-in-dubai-thats-going-places/?outputType=amp"
-          rel="noreferrer"
-        >
-          <div className="card-item">
-            <div className="icon">
-              <img alt="icon" src={f5} />
-            </div>
-            <div className="text">Interview with Sarisha Ved, F5 Founder</div>
-          </div>
-        </a>
+
         <a
           target="_blank"
           href="https://apparelgroupglobal.com/en/wp-content/uploads/2022/10/SustainbilityReport.pdf"
@@ -136,44 +121,6 @@ export default function Card() {
               <img alt="icon" src={tree} />
             </div>
             <div className="text">Apparel Group ESG REPORT 2021</div>
-          </div>
-        </a>
-
-        <a
-          target="_blank"
-          href="https://www.eventbrite.com/e/yoga-by-birkenstock-tickets-463455025457"
-          rel="noreferrer"
-        >
-          <div className="card-item">
-            <div className="icon">
-              <img alt="icon" src={yoga} />
-            </div>
-            <div className="text">Yoga with Birkenstock</div>
-          </div>
-        </a>
-
-        <a
-          target="_blank"
-          href="https://www.eventbrite.com/e/zumba-by-hoka-at-dubai-hills-mall-tickets-466212633537"
-          rel="noreferrer"
-        >
-          <div className="card-item">
-            <div className="icon">
-              <img alt="icon" src={zumba} />
-            </div>
-            <div className="text">Zumba with HOKA</div>
-          </div>
-        </a>
-        <a
-          target="_blank"
-          href="https://www.eventbrite.com/e/yoga-by-havaianas-at-dubai-hills-mall-tickets-466217447937?fbclid=PAAaZK7l4GNunfV_A-nrKaZiJQtoB0v-Q1IjJTsdv93m0z4X23OsGGaT90JCM"
-          rel="noreferrer"
-        >
-          <div className="card-item">
-            <div className="icon">
-              <img alt="icon" src={yoga} />
-            </div>
-            <div className="text">Yoga with Havaianas</div>
           </div>
         </a>
       </div>
