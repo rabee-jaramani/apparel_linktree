8b1096922422567760ee1648c89bc690fe2b338d
Rabee Aljaramani <rabeejermani@gmail.com>
2023-11-14 16:19:15 +0400

    Update Card.js

--- src/card/Card.js
+++ src/card/Card.js
@@ -1,301 +1,142 @@
-<table
-  style="width:100%; "
-  class="easy-table easy-table-default "
-  border="0"
-  id="myTable"
->
-  <thead>
-    <tr>
-      <th style="width:25px;text-align:left">Location</th>
-      <th style="width:100px;text-align:left">Mall</th>
-      <th style="width:40px;text-align:left">Contact</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr style="background-color:#c4c4c4">
-      <td colspan="3" style="text-align:left;">
-        <strong> UAE</strong>
-      </td>
-    </tr>
-    <tr>
-      <td>ABU DHABI, UAE</td>
-      <td>AL RAHA MALL</td>
-      <td>
-        <a href="tel:+971547932309">+971547932309</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>ABU DHABI, UAE</td>
-      <td>KHALIDIYAH MALL</td>
-      <td>
-        <a href="tel:+971547932006">+971547932006</a>
-      </td>
-    </tr>
-    <tr>
-      <td>ABU DHABI, UAE</td>
-      <td>BAWABAT AL SHARQ MALL-POPUP</td>
-      <td>
-        <a href="tel:+971547932345">+971547932345</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>ABU DHABI, UAE</td>
-      <td>DALMA MALL</td>
-      <td>
-        <a href="tel:+971526078518">+971526078518</a>
-      </td>
-    </tr>
-    <tr>
-      <td>ABU DHABI, UAE</td>
-      <td>WTC MALL</td>
-      <td>
-        <a href="tel:+971547932169">+971547932169</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>DUBAI, UAE</td>
-      <td>AL GHURAIR CENTRE</td>
-      <td>
-        <a href="tel:+971547932173">+971547932173</a>
-      </td>
-    </tr>
-    <tr>
-      <td>DUBAI, UAE</td>
-      <td>DUBAI FESTIVAL CITY</td>
-      <td>
-        <a href="tel:+971547932334">+971547932334</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>DUBAI, UAE</td>
-      <td>IBN BATUTA MALL</td>
-      <td>
-        <a href="tel:+971547932235">+971547932235</a>
-      </td>
-    </tr>
-    <tr>
-      <td>DUBAI, UAE</td>
-      <td>MERAAS OUTLET VILLAGE</td>
-      <td>
-        <a href="tel:+971547932171">+971547932171</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>DUBAI, UAE</td>
-      <td>CITY CENTRE DEIRA</td>
-      <td>
-        <a href="tel:+971547932245">+971547932245</a>
-      </td>
-    </tr>
-    <tr>
-      <td>DUBAI, UAE</td>
-      <td>DUBAI HILLS</td>
-      <td>
-        <a href="tel:+971526078612">+971526078612</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>SHARJAH, UAE</td>
-      <td>CITY CENTRE SHARJAH</td>
-      <td>
-        <a href="tel:+971547932172">+971547932172</a>
-      </td>
-    </tr>
-    <tr>
-      <td>DUBAI, UAE</td>
-      <td>FIRST AVENUE MALL</td>
-      <td>
-        <a href="tel:+971552136323">+971552136323</a>
-      </td>
-    </tr>
+import React from 'react';
+import ag_website_icon from '../images/ag_website_icon.png';
+import instagram from '../images/instagram.png';
+import linkedin from '../images/linkedin.png';
+import careers from '../images/careers.png';
+import app_store from '../images/app_store.png';
+import google_play from '../images/google_play.png';
+import tree from '../images/tree.svg';
+import ag_logo from '../images/ag_logo.png';
+export default function Card() {
+  return (
+    <div className="card-container">
+      <div className="main-logo">
+        <img alt="icon" src={ag_logo} />
+      </div>
+      <div className="items-container">
+        <a
+          target="_blank"
+          href="https://www.apparelgroupglobal.com/en/"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={ag_website_icon} />
+            </div>
+            <div className="text">Apparel Group Website </div>
+          </div>
+        </a>
+        <a
+          target="_blank"
+          href="https://www.instagram.com/apparelgroupofficial/"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={instagram} />
+            </div>
+            <div className="text">Apparel Group Instagram </div>
+          </div>
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/company/apparel-fzco/"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={linkedin} />
+            </div>
+            <div className="text">Apparel Group Linkedin</div>
+          </div>
+        </a>
+        <a
+          target="_blank"
+          href="https://ediu.fa.em2.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={careers} />
+            </div>
+            <div className="text">Careers</div>
+          </div>
+        </a>
+        <a
+          target="_blank"
+          href="https://www.instagram.com/clubapparel/"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={instagram} />
+            </div>
+            <div className="text">Club Apparel Instagram</div>
+          </div>
+        </a>
+        <a
+          target="_blank"
+          href="https://play.google.com/store/apps/details?id=com.appemirates.clubapparel&hl=en"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={google_play} />
+            </div>
+            <div className="text">Download on Android</div>
+          </div>
+        </a>
+        <a
+          target="_blank"
+          href="https://apps.apple.com/ae/app/club-apparel/id936671343"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={app_store} />
+            </div>
+            <div className="text">Download on IOS</div>
+          </div>
+        </a>
+        <a
+          target="_blank"
+          href="https://www.instagram.com/thesimaved/"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={instagram} />
+            </div>
+            <div className="text">Meet our founder Mrs. Sima Ved</div>
+          </div>
+        </a>
 
-    <tr style="background-color: #eee;">
-      <td>SHARJAH, UAE</td>
-      <td>MEGA MALL</td>
-      <td>
-        <a href="tel:+971547932293">+971547932293</a>
-      </td>
-    </tr>
-    <tr>
-      <td>SHARJAH, UAE</td>
-      <td>SAHARA CENTRE</td>
-      <td>
-        <a href="tel:+971552136747">+971552136747</a>
-      </td>
-    </tr>
+        <a
+          target="_blank"
+          href="https://apparelgroupglobal.com/en/wp-content/uploads/2022/10/SustainbilityReport.pdf"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={tree} />
+            </div>
+            <div className="text">Apparel Group ESG REPORT 2021</div>
+          </div>
+        </a>
 
-    <tr style="background-color:#c4c4c4">
-      <td colspan="3" style="text-align:left;">
-        <strong> KSA</strong>
-      </td>
-    </tr>
-    <tr>
-      <td>DAMMAM, KSA</td>
-      <td>BBZ DAREEN MALL</td>
-      <td>
-        <a href="tel:+966542842837">+966542842837</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>DAMMAM, KSA</td>
-      <td>AL KIFAH PLAZA</td>
-      <td>
-        <a href="tel:+966544251981">+966544251981</a>
-      </td>
-    </tr>
-    <tr>
-      <td>DAMMAM, KSA</td>
-      <td>OTHAIM MALL</td>
-      <td>
-        <a href="tel:"></a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>JEDDAH, KSA</td>
-      <td>AZIZ MALL</td>
-      <td>
-        <a href="tel:+966112170026">+966112170026</a>
-      </td>
-    </tr>
-    <tr>
-      <td>JEDDAH, KSA</td>
-      <td>HAIFA MALL</td>
-      <td>
-        <a href="tel:+966509353668">+966509353668</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>RIYADH, KSA</td>
-      <td>AL QASR MALL</td>
-      <td>
-        <a href="tel:+966567301484">+966567301484</a>
-      </td>
-    </tr>
-    <tr>
-      <td>RIYADH, KSA</td>
-      <td>ATYAF MALL</td>
-      <td>
-        <a href="tel:+966542842353">+966542842353</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>RIYADH, KSA</td>
-      <td>LOCALIZER MALL</td>
-      <td>
-        <a href="tel:+966534417033">+966534417033</a>
-      </td>
-    </tr>
-    <tr>
-      <td>Hofuf, KSA</td>
-      <td>Lulu Hypermarket</td>
-      <td>
-        <a href=""></a>
-      </td>
-    </tr>
-
-    <tr style="background-color:#c4c4c4">
-      <td colspan="3" style="text-align:left;">
-        <strong> KUWAIT</strong>
-      </td>
-    </tr>
-    <tr>
-      <td>Kuwait</td>
-      <td>ALKOUT MALL</td>
-      <td>
-        <a href="tel:+96596737641">+96596737641</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>Kuwait</td>
-      <td>AL RAI</td>
-      <td>
-        <a href="tel:+96565542569">+96565542569</a>
-      </td>
-    </tr>
-    <tr>
-      <td>Kuwait</td>
-      <td>HAWALLY TUNIS STREET</td>
-      <td>
-        <a href="tel:+96596968978">+96596968978</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>Kuwait</td>
-      <td>BOULEVARD MALL</td>
-      <td>
-        <a href="tel:+96565986444">+96565986444</a>
-      </td>
-    </tr>
-    <tr>
-      <td>Kuwait</td>
-      <td>SULAIL AL JAHRA MALL</td>
-      <td>
-        <a href="tel:+96594459461">+96594459461</a>
-      </td>
-    </tr>
-    <tr style="background-color:#c4c4c4">
-      <td colspan="3" style="text-align:left;">
-        <strong> QATAR</strong>
-      </td>
-    </tr>
-    <tr>
-      <td>QATAR</td>
-      <td>GULF MALL</td>
-      <td>
-        <a href="tel:+97466954831">+97466954831</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>QATAR</td>
-      <td>EZDAN MALL AL WAKRAH</td>
-      <td>
-        <a href="tel:+97466753181">+97466753181</a>
-      </td>
-    </tr>
-    <tr>
-      <td>QATAR</td>
-      <td>Place Vendome</td>
-      <td>
-        <a href="tel:+97455572629">+97455572629</a>
-      </td>
-    </tr>
-    <tr style="background-color:#c4c4c4">
-      <td colspan="3" style="text-align:left;">
-        <strong> OMAN</strong>
-      </td>
-    </tr>
-    <tr>
-      <td>OMAN</td>
-      <td>CITY CENTRE MUSCAT</td>
-      <td>
-        <a href="tel:+96872337028">+96872337028</a>
-      </td>
-    </tr>
-    <tr style="background-color: #eee;">
-      <td>OMAN</td>
-      <td>MALL OF OMAN</td>
-      <td>
-        <a href="tel:+96872442509">+96872442509</a>
-      </td>
-    </tr>
-    <tr>
-      <td>OMAN</td>
-      <td>OMAN AVENUES MALL</td>
-      <td>
-        <a href="tel:+968 24825777">+968 24825777</a>
-      </td>
-    </tr>
-    <tr style="background-color:#c4c4c4">
-      <td colspan="3" style="text-align:left;">
-        <strong> BAHRAIN</strong>
-      </td>
-    </tr>
-    <tr>
-      <td>BAHRAIN</td>
-      <td>SEEF MALL </td>
-      <td>
-        <a href="tel:+97333532226">+97333532226</a>
-      </td>
-    </tr>
-  </tbody>
-</table>;
+        <a
+          target="_blank"
+          href="https://apparelglobal.com/en/wp-content/uploads/2023/11/esg-report-2023-bdf.pdf"
+          rel="noreferrer"
+        >
+          <div className="card-item">
+            <div className="icon">
+              <img alt="icon" src={tree} />
+            </div>
+            <div className="text">Apparel Group ESG REPORT 2022</div>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
